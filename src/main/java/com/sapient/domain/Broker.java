c6df1019b263100b2ac45f0639a10180a3cba62d
Shivanshu-Malik <shivanshumalik2793@gmail.com>
2017-05-13 23:18:43 +0530

    Removed Broker and added Portfolio Manager bean

--- src/main/java/com/sapient/domain/Broker.java
+++
@@ -1,7 +0,0 @@
-package com.sapient.domain;
-
-/**
- * Created by shiva on 5/7/2017.
- */
-public class Broker {
-}
---
+++ src/main/java/com/sapient/domain/PortfolioManager.java
@@ -0,0 +1,7 @@
+package com.sapient.domain;
+
+/**
+ * Created by shiva on 5/13/2017.
+ */
+public class PortfolioManager {
+}
